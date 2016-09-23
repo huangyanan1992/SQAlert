06925c074590aa10b599999f0eb23835681087f7
SQHyn1992 <huangyanan@01zhuanche.com>
2016-09-23 13:46:50 +0800

    Initial Commit

---
+++ SQAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQAlert
+//
+//  Created by Huang Yanan on 2016/9/23.
+//  Copyright © 2016年 Huang Yanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQAlert
+//
+//  Created by Huang Yanan on 2016/9/23.
+//  Copyright © 2016年 Huang Yanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
